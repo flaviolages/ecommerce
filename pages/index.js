465eb618c26f9f314a173ea3e01f38f786023798
flavio lages <meflaviolages@gmail.com>
2021-05-02 17:41:52 -0300

    NextJS + TailwindCSS

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div className="flex flex-row h-screen bg-gray-600">
+      <Head>
+        <title>e-commerce</title>
+        <meta name="description" content="e-commerce" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+
+    </div>
+  )
+}
