e79e742f3d41a3e9cabc798aeb0f74b5ec1632e5
flavio lages <meflaviolages@gmail.com>
2021-05-02 17:49:30 -0300

    v2 Header + framer-motion

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,16 @@
 import Head from 'next/head'
+import Header from '../components/Header/Header';
 
 export default function Home() {
   return (
-    <div className="flex flex-row h-screen bg-gray-600">
+    <div className="flex flex-col h-screen bg-gray-600">
       <Head>
         <title>e-commerce</title>
         <meta name="description" content="e-commerce" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
+      <Header></Header>
 
     </div>
   )
